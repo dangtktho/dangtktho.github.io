916386ba52d500d4be6de71b928033f3534c9291
dangtktho <dangkimtho@gmail.com>
2023-04-10 15:49:58 +0900

    OINrevise

--- OIN-Jewelry/js/script.js
+++ OIN-Jewelry/js/script.js
@@ -59,13 +59,13 @@ $(window).on('load', function () {
 
 //リンク先のidまでスムーススクロール
 //※ページ内リンクを行わない場合は不必要なので削除してください
-    var headerH = $("#header").outerHeight(true);//headerの高さを取得    
-    $('#g-nav li a').click(function () {
-  var elmHash = $(this).attr('href'); 
-  var pos = $(elmHash).offset().top-headerH;//header分の高さを引いた高さまでスクロール
-  $('body,html').animate({scrollTop: pos}, 1000);
-  return false;
-});
+//     var headerH = $("#header").outerHeight(true);//headerの高さを取得    
+//     $('#g-nav li a').click(function () {
+//   var elmHash = $(this).attr('href'); 
+//   var pos = $(elmHash).offset().top-headerH;//header分の高さを引いた高さまでスクロール
+//   $('body,html').animate({scrollTop: pos}, 1000);
+//   return false;
+// });
 
 //slide
         $('.main-slider').bxSlider({
